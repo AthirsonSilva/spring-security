b4183384a6a10e0520a8cac2928cafd29efeb85c
Athirson Silva <Athirsonarceus@gmail.com>
2023-02-12 15:57:39 -0300

    Feat: User register method

---
+++ src/main/java/com/security/app/auth/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.security.app.auth;
+
+import com.security.app.config.JwtService;
+import com.security.app.user.Role;
+import com.security.app.user.User;
+import com.security.app.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        return null;
+    }
+}
